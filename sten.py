9fccd6c0b37ff79d4a66b3098c05766e2522d6ac
eshima0 <linus.sveed@elev.ga.ntig.se>
2022-04-04 12:59:46 +0200

    🚀✅ pepeswag B) (KLAR MED SPEL)

--- sten.py
+++ sten.py
@@ -1,20 +1,34 @@
-from ast import Compare
-from asyncore import loop
 import random
+
+poängspelare = 0
+poängdator = 0
 list = ["sten", "sax", "påse"]
-anwsers2 = ["facebook mamma", "denna kåd är based", "Pojke", "linus L skulle inte ha fåt en tie", "inte ett agel moment", ""]
+answers2 = ["facebook mamma", "denna kod är based", "Pojke", "linus L skulle inte ha fått en tie", "inte ett agel moment"
+, "egg"]
 svar = input("Välkommen, vill du spela ett spel av sten sax påse Y/N?")
-while svar.lower == "y":
-    horny = input("Välj Sten, sax eller påse")
+while svar.lower() == "y":
+    horny = input("Välj Sten, sax eller påse:\n")
+    horny = horny.lower()
     einstein = random.choice(list)
     if horny == einstein:
-        print (random.choice(anwsers2))
+        print(random.choice(answers2))
     elif horny == "sten" and einstein == "sax" or horny == "påse" and einstein == "sten" or horny == "sax" and einstein == "påse":
-        print()
+        print("Du vann :D")
+        poängspelare = poängspelare+1
+        print(f'du har: {poängspelare} poäng :D\n')
     else:
-        print()
-        input("play again? Y/N")
-        if 
+        print("du FÖRLORA >:))))")
+        poängdator = poängdator+1
+        print(f'DATORN har: {poängdator} poäng >:)\n')
+        # input("play again? Y/N")
+        # if 
+    if poängspelare >= 2:
+        print("DU VANN :DDDD stiligt")
+        exit()
+    if poängdator >= 2:
+        print("du förlora >:((( egg")
+        exit()
+    
 
 
 
