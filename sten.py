7df50008634858faf269b78056d1714566dbb0a6
eshima0 <linus.sveed@elev.ga.ntig.se>
2022-04-04 13:34:04 +0200

    🎨 ser mer "pepeswag" ut

--- sten.py
+++ sten.py
@@ -1,35 +1,52 @@
 import random
+import time
 
 poängspelare = 0
 poängdator = 0
 list = ["sten", "sax", "påse"]
 answers2 = ["facebook mamma", "denna kod är based", "Pojke", "linus L skulle inte ha fått en tie", "inte ett agel moment"
 , "egg"]
-svar = input("Välkommen, vill du spela ett spel av sten sax påse Y/N?")
-while svar.lower() == "y":
-    horny = input("Välj Sten, sax eller påse:\n")
-    horny = horny.lower()
-    einstein = random.choice(list)
-    if horny == einstein:
+svar = input("Välkommen, vill du spela ett spel av sten sax påse [Y]/[n]?\n")
+
+while svar.lower() != "n":
+    spelarval = input("Välj Sten, sax eller påse:\n")
+    spelarval = spelarval.lower()
+    datorval = random.choice(list)
+    if spelarval == datorval:
         print(random.choice(answers2))
-    elif horny == "sten" and einstein == "sax" or horny == "påse" and einstein == "sten" or horny == "sax" and einstein == "påse":
-        print("Du vann :D")
+    elif spelarval == "sten" and datorval == "sax" or spelarval == "påse" and datorval == "sten" or spelarval == "sax" and datorval == "påse":
+        print("\nDu vann :D")
         poängspelare = poängspelare+1
         print(f'du har: {poängspelare} poäng :D\n')
     else:
-        print("du FÖRLORA >:))))")
+        print("\ndu FÖRLORA >:))))")
         poängdator = poängdator+1
         print(f'DATORN har: {poängdator} poäng >:)\n')
-        # input("play again? Y/N")
-        # if 
+
     if poängspelare >= 2:
         print("DU VANN :DDDD stiligt")
-        exit()
+        time.sleep(1)
+        igen = input("ska vi spela igen!? [Y]/[N]\n")
+        if igen.lower() == "y":
+            print("okidoki! :D")
+            poängdator = 0
+            poängspelare = 0
+        if igen.lower() == "n":
+            print("Noobium.")
+            exit()
+#egg
     if poängdator >= 2:
-        print("du förlora >:((( egg")
-        exit()
+        print("DATORN vann >:) (noobium)")
+        time.sleep(1)
+        igen = input("ska vi spela igen!? [Y]/[N]\n")
+        if igen.lower() == "y":
+            print("okidoki! :D")
+            poängdator = 0
+            poängspelare = 0
+        if igen.lower() == "n":
+            print("Noobium.")
+            exit()
     
-
 
 
 
